e2684c1a7e328908e8dda4bca0b887490d038422
sobhila.venkataramana <sobhila.venkataramana@gmail.com>
2023-04-24 23:57:26 +0530

    front end public ip

--- servers.tf
+++ servers.tf
@@ -21,6 +21,15 @@ output "frontend" {
   value = aws_instance.frontend.public_ip
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z00437612FVCR3985T5TL"
+  name    = "frontend-dev.priyavenkat.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
+
 resource "aws_instance" "mongodb" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
@@ -28,6 +37,14 @@ resource "aws_instance" "mongodb" {
   tags = {
     Name = "mongodb"
   }
+}
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z00437612FVCR3985T5TL"
+  name    = "mongodb-dev.priyavenkat.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
 }
 
 resource "aws_instance" "catalogue" {
@@ -39,6 +56,14 @@ resource "aws_instance" "catalogue" {
   }
 }
 
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z00437612FVCR3985T5TL"
+  name    = "catalogue-dev.priyavenkat.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
+}
+
 resource "aws_instance" "redis" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
@@ -46,6 +71,14 @@ resource "aws_instance" "redis" {
   tags = {
     Name = "redis"
   }
+}
+
+resource "aws_route53_record" "redis" {
+  zone_id = "Z00437612FVCR3985T5TL"
+  name    = "redis-dev.priyavenkat.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
 }
 
 resource "aws_instance" "user" {
@@ -57,6 +90,14 @@ resource "aws_instance" "user" {
   }
 }
 
+resource "aws_route53_record" "user" {
+  zone_id = "Z00437612FVCR3985T5TL"
+  name    = "user-dev.priyavenkat.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
+}
+
 resource "aws_instance" "cart" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
@@ -64,6 +105,14 @@ resource "aws_instance" "cart" {
   tags = {
     Name = "cart"
   }
+}
+
+resource "aws_route53_record" "cart" {
+  zone_id = "Z00437612FVCR3985T5TL"
+  name    = "cart-dev.priyavenkat.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
 }
 
 resource "aws_instance" "mysql" {
@@ -75,6 +124,14 @@ resource "aws_instance" "mysql" {
   }
 }
 
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z00437612FVCR3985T5TL"
+  name    = "mysql-dev.priyavenkat.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
+
 resource "aws_instance" "shipping" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
@@ -82,6 +139,14 @@ resource "aws_instance" "shipping" {
   tags = {
     Name = "shipping"
   }
+}
+
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z00437612FVCR3985T5TL"
+  name    = "shipping-dev.priyavenkat.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
 }
 
 resource "aws_instance" "rabbitmq" {
@@ -93,6 +158,14 @@ resource "aws_instance" "rabbitmq" {
   }
 }
 
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z00437612FVCR3985T5TL"
+  name    = "rabbitmq-dev.priyavenkat.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
+
 resource "aws_instance" "payment" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
@@ -100,6 +173,14 @@ resource "aws_instance" "payment" {
   tags = {
     Name = "payment"
   }
+}
+
+resource "aws_route53_record" "payment" {
+  zone_id = "Z00437612FVCR3985T5TL"
+  name    = "payment-dev.priyavenkat.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
 }
 
 resource "aws_instance" "dispatch" {
@@ -111,3 +192,10 @@ resource "aws_instance" "dispatch" {
   }
 }
 
+resource "aws_route53_record" "dispatch" {
+  zone_id = "Z00437612FVCR3985T5TL"
+  name    = "dispatch-dev.priyavenkat.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.dispatch.private_ip]
+}
