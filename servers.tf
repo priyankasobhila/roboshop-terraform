ce2cc972fa112c626ae28260f3fa45f12a42b42b
sobhila.venkataramana <sobhila.venkataramana@gmail.com>
2023-05-02 23:31:48 +0530

    front end public ip

--- servers.tf
+++ servers.tf
@@ -60,14 +60,15 @@ variable  "components"{
 
 resource "aws_instance" "instance" {
   for_each = var.components
-}
- ami           = data.aws_ami.centos.image_id
-  instance_type = each.value["instance_type"]
+
+  ami                    = data.aws_ami.centos.image_id
+  instance_type          = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
     Name = each.value["name"]
- }
+  }
+}
 
 //}
 //resource "aws_route53_record" "frontend" {
