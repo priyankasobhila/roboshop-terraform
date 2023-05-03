0831ddba335022a28a2c4bb3a1e337a215859c86
sobhila.venkataramana <sobhila.venkataramana@gmail.com>
2023-05-03 23:31:27 +0530

    state

--- servers.tf
+++ servers.tf
@@ -1,61 +1,10 @@
-data "aws_ami" "centos" {
-  most_recent = true
-  owners = ["973714476881"]
-  name_regex  = "Centos-8-DevOps-Practice"
-}
 
-data "aws_security_group" "allow-all" {
-  name = "allow-all"
-}
 
 variable "instance_type" {
   default = "t3.small"
 }
 
-variable  "components"{
-  default = {
-    frontend = {
-      name = "frontend"
-      instance_type = "t3.micro"
-    }
-    mongodb = {
-      name = "mongodb"
-      instance_type = "t3.micro"
-    }
-    catalogue = {
-      name = "catalogue"
-      instance_type = "t3.micro"
-    }
-    redis = {
-      name = "redis"
-      instance_type = "t3.micro"
-    }
-    user = {
-      name = "user"
-      instance_type = "t3.micro"
-    }
-    cart = {
-      name = "cart"
-      instance_type = "t3.micro"
-    }
-    mysql = {
-      name = "mysql"
-      instance_type = "t3.micro"
-    }
-    rabbitmq = {
-      name = "rabbitmq"
-      instance_type = "t3.micro"
-    }
-    shipping = {
-      name = "shipping"
-      instance_type = "t3.medium"
-    }
-    payment = {
-      name = "payment"
-      instance_type = "t3.micro"
-    }
-  }
-}
+
 
 
 resource "aws_instance" "instance" {
