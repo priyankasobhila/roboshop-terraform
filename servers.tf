2da2b7806354645d75d98513b6fe3c4c72ccce8f
sobhila.venkataramana <sobhila.venkataramana@gmail.com>
2023-05-03 23:44:48 +0530

    state

--- servers.tf
+++ servers.tf
@@ -4,11 +4,9 @@ variable "instance_type" {
 
 resource "aws_instance" "instance" {
   for_each = var.components
-
   ami                    = data.aws_ami.centos.image_id
   instance_type          = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
-
   tags = {
     Name = each.value["name"]
   }
