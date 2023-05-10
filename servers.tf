413beffb960f01b74c3699eb199f401e4c8e7eae
sobhila.venkataramana <sobhila.venkataramana@gmail.com>
2023-05-10 14:57:24 +0530

    addes

--- servers.tf
+++ servers.tf
@@ -1,190 +1,21 @@
-resource "aws_instance" "instance" {
-  for_each               = var.components
-  ami                    = data.aws_ami.centos.image_id
-  instance_type          = each.value["instance_type"]
-  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+module "database-servers" {
+  for_each = var.database_servers
 
-  tags = {
-    Name = each.value["name"]
-  }
-
-}
-resource "null_resource" "provisioner" {
-    depends_on = [aws_instance.instance,aws_route53_record.records]
-    for_each               = var.components
-  provisioner "remote-exec" {
-
-    connection {
-      type     = "ssh"
-      user     = "centos"
-      password = "DevOps321"
-      host     = aws_instance.instance[each.value["name"]].private_ip
-    }
-
-    inline = [
-      "rm -rf roboshop-shell",
-      "git clone https://github.com/priyavenkat/Roboshop-shell",
-      "cd Roboshop-shell",
-      "sudo bash ${each.value["name"]}.sh ${lookup(each.value, "password", "dummy")}"
-    ]
-  }
+  source         = "./module"
+  component_name = each.value["name"]
+  env            = var.env
+  instance_type  = each.value["instance_type"]
+  password       = lookup(each.value, "password", "null")
+  provisioner    = true
 }
 
-resource "aws_route53_record" "records" {
-  for_each = var.components
-  zone_id  = "Z00437612FVCR3985T5TL"
-  name     = "${each.value["name"]}-dev.priyavenkat.online"
-  type     = "A"
-  ttl      = 30
-  records  = [aws_instance.instance[each.value["name"]].private_ip]
+module "app-servers" {
+  depends_on = [module.database-servers]
+  for_each   = var.app_servers
+
+  source         = "./module"
+  component_name = each.value["name"]
+  env            = var.env
+  instance_type  = each.value["instance_type"]
+  password       = lookup(each.value, "password", "null")
 }
-
-
-//resource "aws_instance" "frontend" {
-//  ami           = data.aws_ami.centos.image_id
-//  instance_type = var.instance_type
-//  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
-//
-//  tags = {
-//    Name = "frontend"
-// }
-//}
-//output "frontend"{
-//  value =aws_instance.frontend.public_ip
-//}
-//resource "aws_instance" "mongodb" {
-//  ami           = data.aws_ami.centos.image_id
-//  instance_type = var.instance_type
-//  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
-//  tags = {
-//    Name = "mongodb"
-// }
-//}
-//resource "aws_route53_record" "mongodb" {
-//  zone_id = "Z00437612FVCR3985T5TL"
-//  name    = "mongodb-dev.priyavenkat.online"
-//  type    = "A"
-//  ttl     = 30
-//records = [aws_instance.mongodb.private_ip]
-//}
-//resource "aws_instance" "catalogue" {
-//  ami           = data.aws_ami.centos.image_id
-//  instance_type = var.instance_type
-//  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
-//  tags = {
-//    Name = "catalogue"
-//  }
-//}
-//resource "aws_route53_record" "catalogue" {
-//  zone_id = "Z00437612FVCR3985T5TL"
-//  name    = "catalogue-dev.priyavenkat.online"
-//  type    = "A"
-//  ttl     = 30
-//  records = [aws_instance.catalogue.private_ip]
-//}
-//resource "aws_instance" "redis" {
-//  ami           = data.aws_ami.centos.image_id
-//  instance_type = var.instance_type
-//  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
-//  tags = {
-//    Name = "redis"
-// }
-//}
-//resource "aws_route53_record" "redis" {
-//  zone_id = "Z00437612FVCR3985T5TL"
-//  name    = "redis-dev.priyavenkat.online"
-//  type    = "A"
-//  ttl     = 30
-//  records = [aws_instance.redis.private_ip]
-//}
-//resource "aws_instance" "cart" {
-//  ami           = data.aws_ami.centos.image_id
-//  instance_type = var.instance_type
-//  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
-//  tags = {
-//    Name = "cart"
-//  }
-//}
-//resource "aws_route53_record" "cart" {
-//  zone_id = "Z00437612FVCR3985T5TL"
-//  name    = "cart-dev.priyavenkat.online"
-//  type    = "A"
-//  ttl     = 30
-//  records = [aws_instance.cart.private_ip]
-//}
-//resource "aws_instance" "mysql" {
-//  ami           = data.aws_ami.centos.image_id
-//  instance_type = var.instance_type
-//  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
-// tags = {
-//    Name = "mysql"
-//  }
-//}
-//resource "aws_route53_record" "mysql" {
-//  zone_id = "Z00437612FVCR3985T5TL"
-//  name    = "mysql-dev.priyavenkat.online"
-//  type    = "A"
-//  ttl     = 30
-//  records = [aws_instance.mysql.private_ip]
-//}
-//resource "aws_instance" "user" {
-//  ami           = data.aws_ami.centos.image_id
-//  instance_type = var.instance_type
-//  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
-//  tags = {
-//    Name = "user"
-//  }
-//}
-//resource "aws_route53_record" "user" {
-//  zone_id = "Z00437612FVCR3985T5TL"
-//  name    = "user-dev.priyavenkat.online"
-//  type    = "A"
-//  ttl     = 30
-//  records = [aws_instance.user.private_ip]
-//}
-//resource "aws_instance" "rabbitmq" {
-//  ami           = data.aws_ami.centos.image_id
-//  instance_type = var.instance_type
-//  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
-//tags = {
-//    Name = "rabbitmq"
-//  }
-//}
-//resource "aws_route53_record" "rabbitmq" {
-//  zone_id = "Z00437612FVCR3985T5TL"
-//  name    = "rabbitmq-dev.priyavenkat.online"
-//  type    = "A"
-//  ttl     = 30
-//  records = [aws_instance.rabbitmq.private_ip]
-//}
-//resource "aws_instance" "shipping" {
-// ami           = data.aws_ami.centos.image_id
-//  instance_type = var.instance_type
-//  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
-//  tags = {
-//    Name = "shipping"
-//  }
-//}
-//resource "aws_route53_record" "shipping" {
-//  zone_id = "Z00437612FVCR3985T5TL"
-//  name    = "shipping-dev.priyavenkat.online"
-//  type    = "A"
-//  ttl     = 30
-//records = [aws_instance.shipping.private_ip]
-//}
-//resource "aws_instance" "payment" {
-//  ami           = data.aws_ami.centos.image_id
-//  instance_type = var.instance_type
-//  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
-//  tags = {
-//    Name = "payment"
-//  }
-//}
-//resource "aws_route53_record" "payment" {
-//  zone_id = "Z00437612FVCR3985T5TL"
-//  name    = "payment-dev.priyavenkat.online"
-//  type    = "A"
-//  ttl     = 30
-//records = [aws_instance.payment.private_ip]
-//}
-
