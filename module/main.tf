1797cceb65d0ed2406fffe1b887cc46057586924
sobhila.venkataramana <sobhila.venkataramana@gmail.com>
2023-05-10 15:08:51 +0530

    addes

---
+++ module/main.tf
@@ -0,0 +1,39 @@
+resource "aws_instance" "instance" {
+  ami                    = data.aws_ami.centos.image_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+
+  tags = {
+    Name = local.name
+  }
+}
+
+resource "null_resource" "provisioner" {
+  count      = var.provisioner ? 1 : 0
+  depends_on = [aws_instance.instance, aws_route53_record.records]
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = aws_instance.instance.private_ip
+    }
+
+    inline = [
+      "rm -rf roboshop-shell",
+      "git clone https://github.com/priyankasobhila/roboshop-shell",
+      "cd roboshop-shell",
+      "sudo bash ${var.component_name}.sh ${var.password}"
+    ]
+  }
+}
+
+
+resource "aws_route53_record" "records" {
+  zone_id = "Z00437612FVCR3985T5TL"
+  name    = "${var.component_name}-dev.priyavenkat.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instance.private_ip]
+}
